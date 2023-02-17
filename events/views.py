c48d66e578636c30f5b9878b138e1bebe72be146
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-02-17 08:32:52 +0100

    adding forms and widgets

--- events/views.py
+++ events/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from datetime import datetime
 import calendar
+from django.http import HttpResponseRedirect
 from calendar import HTMLCalendar
+
 from .models import Event
-
-
+from .forms import VenueForm 
 now = datetime.now()
 
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'events/add_venue.html',
+    {'form' : form, 'submitted' : submitted})
 
 def all_events(request):
     event_list = Event.objects.all()
